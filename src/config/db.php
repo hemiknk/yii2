bfa015a5bb4a96dd601359bd83be596c46591bf1
artem <tema.tarasow@gmail.com>
2016-12-07 17:49:24 +0200

    delete old po file

--- src/config/db.php
+++ src/config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=vagrant_yii2_template',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
